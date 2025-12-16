78709019c75aec33a4114d23e5cb1445c05a83cb
Davinder498 <davinder.silver@gmail.com>
2025-12-16 01:43:43 -0700

    Change SPARSH_SERVICE_EMAIL for testing
    Updated the service email for testing purposes.

--- constants.ts
+++ constants.ts
@@ -3,4 +3,4 @@
 
 // TODO: Change this email address to your personal email for testing purposes.
 // When ready for production, revert it to 'sparshnri.dad@gov.in'.
-export const SPARSH_SERVICE_EMAIL = "sparshnri.dad@gov.in";
+export const SPARSH_SERVICE_EMAIL = "davinder.gold@yahoo.co.in";
